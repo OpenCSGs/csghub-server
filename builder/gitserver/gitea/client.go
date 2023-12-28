20ac5538ce7a343e560ac074b2a7ef91696b908a
泽华 <zh.zhang@opencsg.com>
2023-12-28 06:28:05 +0000

    Add git_server_access_tokens table

--- builder/gitserver/gitea/client.go
+++ builder/gitserver/gitea/client.go
@@ -1,9 +1,18 @@
 package gitea
 
 import (
+	"bytes"
 	"context"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log/slog"
+	"net/http"
 
 	"github.com/pulltheflower/gitea-go-sdk/gitea"
+	"opencsg.com/starhub-server/builder/store/database"
 	"opencsg.com/starhub-server/common/config"
 )
 
@@ -18,12 +27,22 @@ type Response struct {
 	Data    any    `json:"data"`
 }
 
+type TokenResponse struct {
+	SHA1    string `json:"sha1"`
+	Message string `json:"message"`
+}
+
 func NewClient(config *config.Config) (client *Client, err error) {
 	ctx := context.Background()
+	token, err := findOrCreateAccessToken(ctx, config)
+	if err != nil {
+		slog.Error("Failed to find or create token", slog.String("error: ", err.Error()))
+		return nil, err
+	}
 	giteaClient, err := gitea.NewClient(
 		config.GitServer.Host,
 		gitea.SetContext(ctx),
-		gitea.SetToken(config.GitServer.SecretKey),
+		gitea.SetToken(token.Token),
 		gitea.SetBasicAuth(config.GitServer.Username, config.GitServer.Password),
 	)
 
@@ -32,4 +51,91 @@ func NewClient(config *config.Config) (client *Client, err error) {
 	}
 
 	return &Client{giteaClient: giteaClient, config: config}, nil
+}
+
+func findOrCreateAccessToken(ctx context.Context, config *config.Config) (*database.GitServerAccessToken, error) {
+	gs := database.NewGitServerAccessTokenStore()
+	tokens, err := gs.Index(ctx)
+	if err != nil {
+		slog.Error("Fail to get git server access token from database", slog.String("error: ", err.Error()))
+		return nil, err
+	}
+
+	if len(tokens) == 0 {
+		access_token, err := generateAccessTokenFromGitea(config)
+		if err != nil {
+			slog.Error("Fail to create git server access token", slog.String("error: ", err.Error()))
+			return nil, err
+		}
+		gToken := &database.GitServerAccessToken{
+			Token: access_token,
+		}
+
+		gToken, err = gs.Create(ctx, gToken)
+		if err != nil {
+			slog.Error("Fail to create git server access token", slog.String("error: ", err.Error()))
+			return nil, err
+		}
+
+		return gToken, nil
+	}
+	return &tokens[0], nil
+}
+
+func encodeCredentials(username, password string) string {
+	credentials := username + ":" + password
+	return base64.StdEncoding.EncodeToString([]byte(credentials))
+}
+
+func generateAccessTokenFromGitea(config *config.Config) (string, error) {
+	username := config.GitServer.Username
+	password := config.GitServer.Password
+	giteaUrl := fmt.Sprintf("%s/api/v1/users/%s/tokens", config.GitServer.URL, username)
+	authHeader := encodeCredentials(username, password)
+	data := map[string]any{
+		"name":   "access_token",
+		"scopes": []string{"read:user", "write:user", "write:admin", "read:admin"},
+	}
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		slog.Error("Error encoding JSON data:", err)
+		return "", err
+	}
+
+	req, err := http.NewRequest("POST", giteaUrl, bytes.NewBuffer(jsonData))
+	if err != nil {
+		slog.Error("Error creating request:", err)
+		return "", err
+	}
+
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Basic "+authHeader)
+
+	client := http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		slog.Error("Error sending request:", err)
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		slog.Error("Error reading response body:", err)
+		return "", err
+	}
+
+	var tokenResponse TokenResponse
+	err = json.Unmarshal(body, &tokenResponse)
+	if err != nil {
+		slog.Error("Error decoding JSON response:", err)
+		return "", err
+	}
+
+	if tokenResponse.Message != "" {
+		return "", errors.New(tokenResponse.Message)
+	}
+
+	return tokenResponse.SHA1, nil
 }
