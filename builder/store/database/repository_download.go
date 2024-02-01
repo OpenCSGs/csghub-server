3fb51f1445b3fb781a3b37cc9545fc3b1489e6f2
zhzhang <pulltheflower@163.com>
2024-02-01 10:30:43 +0800

    Rename download_count to click_download_count

--- builder/store/database/repository_download.go
+++ builder/store/database/repository_download.go
@@ -3,11 +3,11 @@ package database
 import "time"
 
 type RepositoryDownload struct {
-	ID            int64       `bun:",pk,autoincrement" json:"id"`
-	RepositoryID  int64       `bun:",notnull" json:"repository_id"`
-	Date          time.Time   `bun:",notnull,type:date" json:"date"`
-	CloneCount    int64       `bun:",notnull" json:"user_id"`
-	DownloadCount int64       `bun:",notnull" json:"download_count"`
-	Repository    *Repository `bun:"rel:belongs-to,join:repository_id=id"`
+	ID                 int64       `bun:",pk,autoincrement" json:"id"`
+	RepositoryID       int64       `bun:",notnull" json:"repository_id"`
+	Date               time.Time   `bun:",notnull,type:date" json:"date"`
+	CloneCount         int64       `bun:",notnull" json:"user_id"`
+	ClickDownloadCount int64       `bun:",notnull" json:"click_download_count"`
+	Repository         *Repository `bun:"rel:belongs-to,join:repository_id=id"`
 	times
 }
