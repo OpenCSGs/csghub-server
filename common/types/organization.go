897d5ddc61f1bedae2b5deec091fa8da083895ae
zhzhang <pulltheflower@163.com>
2024-02-18 10:53:53 +0800

    Check current user when deleting and updating organization

--- common/types/organization.go
+++ common/types/organization.go
@@ -15,7 +15,13 @@ type EditOrgReq struct {
 	Description string `json:"description" example:"org description"`
 	//TODO:rename json field name to 'name", need to negotiate with Portal engineer
 	//Org unique identifier
-	Name string `json:"path" example:"org_name_1"`
+	Name        string `json:"-"`
+	CurrentUser string `json:"current_user"`
+}
+
+type DeleteOrgReq struct {
+	Name        string `json:"-"`
+	CurrentUser string `json:"current_user"`
 }
 
 type OrgDatasetsReq struct {
