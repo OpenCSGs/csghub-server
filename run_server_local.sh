b1c0192e44307c7e9eae991ca9b74d3c5d6e9f4c
zhzhang <pulltheflower@163.com>
2024-02-19 17:51:56 +0800

    Update run_server_local.sh

--- run_server_local.sh
+++ run_server_local.sh
@@ -1,37 +1,46 @@
 #!/bin/bash
 
-export STARHUB_DATABASE_DSN="postgresql://postgres:postgres@localhost:5433/starhub_server?sslmode=disable"
-export STARHUB_DATABASE_TIMEZONE="Asia/Shanghai"
-export STARHUB_SERVER_REDIS_ENDPOINT="localhost:6379"
-export STARHUB_SERVER_GITSERVER_HOST="http://localhost:3001"
-# export STARHUB_SERVER_GITSERVER_SECRET_KEY="46389362f3c3e2aadc6083b94c0f226337a98b6d"
-# export STARHUB_SERVER_GITSERVER_HOST="http://localhost:3000"
-export STARHUB_SERVER_GITSERVER_SECRET_KEY="cbcfa2497b51a6f75adcff8421a6b8da808fe505"
-export STARHUB_SERVER_GITSERVER_USERNAME="root"
-export STARHUB_SERVER_GITSERVER_PASSWORD="password123"
-# export STARHUB_SERVER_GITSERVER_USERNAME="leida"
-# export STARHUB_SERVER_GITSERVER_PASSWORD="12345678"
-export GITEA_SERVICE_DEFAULT_ALLOW_CREATE_ORGANIZATION=true
-export POSTGRES_USER="postgres"
-export POSTGRES_PASSWORD="postgres"
-export POSTGRES_DB="starhub_server"
-export GITEA_USERNAME="root"
-export GITEA_PASSWORD="password123"
+export STARHUB_DATABASE_DSN=postgresql://postgres:postgres@localhost:5433/starhub_server?sslmode=disable
+export STARHUB_DATABASE_TIMEZONE=Asia/Shanghai
+export STARHUB_SERVER_GITSERVER_HOST=http://localhost:3001
+export STARHUB_SERVER_GITSERVER_URL=http://localhost:3001
+export STARHUB_SERVER_GITSERVER_USERNAME=root
+export STARHUB_SERVER_GITSERVER_PASSWORD=password123
+export STARHUB_SERVER_GITSERVER_WEBHOOK_URL=http://localhost:8080/api/v1/callback/git
+export POSTGRES_USER=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_DB=starhub_server
+export GITEA_USERNAME=root
+export GITEA_PASSWORD=password123
+export GIN_MODE=release
+#  export STARHUB_SERVER_API_TOKEN=$STARHUB_SERVER_API_TOKEN
+export STARHUB_SERVER_S3_ACCESS_KEY_ID=$STARHUB_SERVER_S3_ACCESS_KEY_ID
+export STARHUB_SERVER_S3_ACCESS_KEY_SECRET=$STARHUB_SERVER_S3_ACCESS_KEY_SECRET
+export STARHUB_SERVER_S3_REGION=$STARHUB_SERVER_S3_REGION
+#  export STARHUB_SERVER_S3_BUCKET: $STARHUB_SERVER_S3_BUCKET
+
 #export GIN_MODE="release"
 
 #allow Bun to log sql queries
 export DB_DEBUG=1
 
-#maker sure you have defined these 3 environment variables
-# export STARHUB_SERVER_ALIYUN_ACCESS_KEY_ID="[YOUR_ACCESS_KEY_ID]"
-# export STARHUB_SERVER_ALIYUN_ACCESS_KEY_SECRET="[YOUR_ACCESS_KEY_SECRET]"
-# export STARHUB_SERVER_ALIYUN_REGION="[YOUR_ALIYUN_REGION]"
+# cd ..
+echo $(pwd)
 
-go build -v -o ./bin/starhub-server ./cmd/starhub-server
+~/go/bin/swag init -d cmd/csghub-server/cmd/start,api/router,api/handler,builder/store/database,common/types -g server.go
 
-./bin/starhub-server migration init
+go build -v -o ./bin/csghub-server ./cmd/csghub-server
+
+# make sure build succeeded
+if [ $? -ne 0 ]; then
+        echo "go build failed"
+        exit 1
+fi
+
+./bin/csghub-server migration init
 #uncomment this command if db schema changed
-# ./bin/starhub-server migration create_sql add_col_count_to_repository_tags
-./bin/starhub-server migration migrate 
+#./bin/csghub-server migration create_sql create_table_spaces
+./bin/csghub-server migration migrate
 
-./bin/starhub-server start server -l DEBUG -f json 
+./bin/csghub-server start server -l DEBUG -f json --swagger true
+#./bin/csghub-server trigger fix-org-data -l DEBUG -f json
