89304f61b4c7ba23da8543d9343ca73c957d7d0d
zhzhang <pulltheflower@163.com>
2024-02-21 17:53:12 +0800

    Fix migration bug

--- builder/store/database/migrations/20240131084249_add_download_count_to_repository_downloads.down.sql
+++ builder/store/database/migrations/20240131084249_add_download_count_to_repository_downloads.down.sql
@@ -2,8 +2,8 @@ SET statement_timeout = 0;
 
 --bun:split
 
-ALTER TABLE repository_downloads DROP COLUMN click_download_count;
+ALTER TABLE repository_downloads DROP COLUMN IF EXISTS click_download_count;
 
 --bun:split
 
-ALTER TABLE repository_downloads RENAME COLUMN clone_count TO count;
+ALTER TABLE repository_downloads RENAME COLUMN IF EXISTS clone_count TO count;
--- builder/store/database/migrations/20240131084249_add_download_count_to_repository_downloads.up.sql
+++ builder/store/database/migrations/20240131084249_add_download_count_to_repository_downloads.up.sql
@@ -2,8 +2,8 @@ SET statement_timeout = 0;
 
 --bun:split
 
-ALTER TABLE repository_downloads ADD click_download_count INT DEFAULT 0;
+ALTER TABLE repository_downloads ADD COLUMN IF NOT EXISTS click_download_count INT DEFAULT 0;
 
 --bun:split
 
-ALTER TABLE repository_downloads RENAME COLUMN count TO clone_count;
+ALTER TABLE repository_downloads RENAME COLUMN IF EXISTS count TO clone_count;
