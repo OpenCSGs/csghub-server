992a087fe60e3cf507d8c522b45de1fb472d329a
rader <ld_leida@hotmail.com>
2024-03-06 09:50:42 +0800

    Reverse proxy (#43)
    * reverse proxy for calling space api
    ---------
    
    Co-authored-by: Lei Da <da.lei@opencsg.com>

--- common/types/jwt.go
+++ common/types/jwt.go
@@ -3,8 +3,8 @@ package types
 import "github.com/golang-jwt/jwt/v5"
 
 type CreateJWTReq struct {
-	CurrentUser   string   `json:"current_user"`
-	Organizations []string `json:"organizations"`
+	CurrentUser   string   `json:"current_user" binding:"required"`
+	Organizations []string `json:"organizations" `
 }
 
 type JWTClaims struct {
