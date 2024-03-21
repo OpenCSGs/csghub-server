e4c717c986ec5988cef211c0b339f5dc2192ede1
Lei Da <da.lei@opencsg.com>
2024-03-21 08:46:38 +0800

    simulator space status change for test only

--- api/handler/space.go
+++ api/handler/space.go
@@ -413,7 +413,16 @@ func (h *SpaceHandler) status(ctx *gin.Context) {
 			return
 		default:
 			time.Sleep(time.Second * 5)
+			ctx.SSEvent("status", "Building")
+			ctx.Writer.Flush()
+			time.Sleep(time.Second * 5)
 			ctx.SSEvent("status", "Running")
+			ctx.Writer.Flush()
+			time.Sleep(time.Second * 5)
+			ctx.SSEvent("status", "Sleeping")
+			ctx.Writer.Flush()
+			time.Sleep(time.Second * 5)
+			ctx.SSEvent("status", "Stop")
 			ctx.Writer.Flush()
 		}
 	}
