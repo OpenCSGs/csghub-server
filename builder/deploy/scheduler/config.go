b50c30129505bfb84346e8246640f38c7bd0c204
Haihui.wang <wanghh2000@163.com>
2024-04-22 11:01:56 +0000

    set resource limits for space deployment

--- builder/deploy/scheduler/config.go
+++ builder/deploy/scheduler/config.go
@@ -4,3 +4,20 @@ type Config struct {
 	ImageBuilderURL string `json:"image_builder_url"`
 	ImageRunnerURL  string `json:"image_runner_url"`
 }
+
+type SDKConfig struct {
+	name    string
+	version string
+	port    string
+}
+
+var GRADIO = SDKConfig{
+	name:    "gradio",
+	version: "3.37.0",
+	port:    "7860",
+}
+var STREAMLIT = SDKConfig{
+	name:    "streamlit",
+	version: "1.33.0",
+	port:    "8501",
+}
