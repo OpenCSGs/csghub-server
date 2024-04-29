3de8739e7da7fdbb014cf716306f4d3f8a893155
Lei Da <da.lei@opencsg.com>
2024-04-29 12:00:18 +0800

    init empty env map if space env is invalid json

--- builder/deploy/common/utils.go
+++ builder/deploy/common/utils.go
@@ -4,6 +4,9 @@ import "encoding/json"
 
 func JsonStrToMap(jsonStr string) (map[string]interface{}, error) {
 	var resMap map[string]interface{}
+	if len(jsonStr) == 0 {
+		return map[string]interface{}{}, nil
+	}
 	err := json.Unmarshal([]byte(jsonStr), &resMap)
 	return resMap, err
 }
