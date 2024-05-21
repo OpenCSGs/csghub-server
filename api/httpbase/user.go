e2140d2914454d573a6e706bb08a488e0f23b01f
Da.Lei <da.lei@opencsg.com>
2024-05-21 01:57:09 +0000

    uniform authentication for all api, support auth with ApiKey,JWT and AccessToken at the same time

--- api/httpbase/user.go
+++ api/httpbase/user.go
@@ -4,7 +4,17 @@ import "github.com/gin-gonic/gin"
 
 const (
 	CurrentUserCtxVar   = "currentUser"
+	AccessTokenCtxVar   = "accessToken"
+	AuthTypeCtxVar      = "authType"
 	CurrentUserQueryVar = "current_user"
+)
+
+type AuthType string
+
+const (
+	AuthTypeApiKey      AuthType = "ApiKey"
+	AuthTypeJwt         AuthType = "JWT"
+	AuthTypeAccessToken AuthType = "AccessToken"
 )
 
 // GetCurrentUser returns the current user name from the context.
@@ -16,4 +26,20 @@ func GetCurrentUser(ctx *gin.Context) string {
 
 func SetCurrentUser(ctx *gin.Context, user string) {
 	ctx.Set(CurrentUserCtxVar, user)
+}
+
+func GetAccessToken(ctx *gin.Context) string {
+	return ctx.GetString(AccessTokenCtxVar)
+}
+
+func SetAccessToken(ctx *gin.Context, user string) {
+	ctx.Set(CurrentUserCtxVar, user)
+}
+
+func GetAuthType(ctx *gin.Context) AuthType {
+	return AuthType(ctx.GetString(AuthTypeCtxVar))
+}
+
+func SetAuthType(ctx *gin.Context, t AuthType) {
+	ctx.Set(AuthTypeCtxVar, string(t))
 }
