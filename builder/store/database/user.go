fc6ca21195c7d03a9670ee1066cf1941ac42a8e3
泽华 <zh.zhang@opencsg.com>
2024-09-09 07:57:05 +0000

    Add admin api/v1/users api

--- builder/store/database/user.go
+++ builder/store/database/user.go
@@ -88,6 +88,25 @@ func (s *UserStore) Index(ctx context.Context) (users []User, err error) {
 	return
 }
 
+func (s *UserStore) IndexWithSearch(ctx context.Context, search string, per, page int) (users []User, count int, err error) {
+	search = strings.ToLower(search)
+	query := s.db.Operator.Core.NewSelect().
+		Model(&users)
+	if search != "" {
+		query.Where("LOWER(username) like ? OR LOWER(email) like ?", fmt.Sprintf("%%%s%%", search), fmt.Sprintf("%%%s%%", search))
+	}
+	count, err = query.Count(ctx)
+	if err != nil {
+		return
+	}
+	query.Order("id asc").Limit(per).Offset((page - 1) * per)
+	err = query.Scan(ctx, &users)
+	if err != nil {
+		return
+	}
+	return
+}
+
 func (s *UserStore) FindByUsername(ctx context.Context, username string) (user User, err error) {
 	user.Username = username
 	err = s.db.Operator.Core.NewSelect().Model(&user).Where("username = ?", username).Scan(ctx)
