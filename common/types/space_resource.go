b6b60d975379dc98c90731f673a3d0c7a187edef
James <370036720@qq.com>
2024-10-14 20:57:54 +0800

    add resource type (#146)

--- common/types/space_resource.go
+++ common/types/space_resource.go
@@ -1,10 +1,18 @@
 package types
 
+type ResourceType string
+
+const (
+	ResourceTypeCPU ResourceType = "cpu"
+	ResourceTypeGPU ResourceType = "gpu"
+)
+
 type SpaceResource struct {
-	ID          int64  `json:"id"`
-	Name        string `json:"name"`
-	Resources   string `json:"resources"`
-	IsAvailable bool   `json:"is_available"`
+	ID          int64        `json:"id"`
+	Name        string       `json:"name"`
+	Resources   string       `json:"resources"`
+	IsAvailable bool         `json:"is_available"`
+	Type        ResourceType `json:"type"`
 }
 
 type CreateSpaceResourceReq struct {
