539596076617d8c123c66515034de0c1cbcd22c9
James <370036720@qq.com>
2025-01-10 13:17:07 +0800

    Support sglang and upgrade swift to latest (#235)
    * upgrade swift
    
    * support sglang
    
    ---------
    
    Co-authored-by: James <xzgan@opencsg.com>

--- docker/inference/sglang/serve.sh
+++ docker/inference/sglang/serve.sh
@@ -3,13 +3,12 @@
 export PYTHONPATH="$(pwd):$PYTHONPATH"
 
 python3 /etc/csghub/entry.py
+
 if [ -z "$GPU_NUM" ]; then
     GPU_NUM=1
 fi
-#LimitedMaxToken is gpu_num multiplied by 4096
 LimitedMaxToken=$(($GPU_NUM * 4096))
-GPU_MEMORY_UTILIZATION=0.9
-args="--trust-remote-code --model $REPO_ID --tensor-parallel-size $GPU_NUM --gpu-memory-utilization $GPU_MEMORY_UTILIZATION"
+args="--tp $GPU_NUM --enable-mixed-chunk --disable-radix-cache --trust-remote-code --enable-p2p-check --model-path $REPO_ID --port 8000 --host 0.0.0.0 --mem-fraction-static 0.8 --enable-torch-compile"
 configfile="/workspace/$REPO_ID/config.json"
 if [ -f "$configfile" ]; then
     MAX_TOKENS=$(grep '"max_position_embeddings"' $configfile | cut -d":" -f2 | sed 's/[^0-9]*//g')
@@ -21,12 +20,12 @@ if [ -f "$configfile" ]; then
         if [ $MAX_TOKENS -gt $LimitedMaxToken ]; then
             MAX_TOKENS=$LimitedMaxToken       
         fi
-        args="$args --max-model-len $MAX_TOKENS"
+        args="$args --context-length $MAX_TOKENS"
     fi
 fi
 tokenizer_config="/workspace/$REPO_ID/tokenizer_config.json"
 if ! grep -q "chat_template" "$tokenizer_config"; then
-    args="$args --chat_template /etc/csghub/chat_template.jinja"
+    args="$args --chat-template /etc/csghub/chat_template.jinja"
 fi
-    
-python3 -m vllm.entrypoints.openai.api_server $args
+echo "start running with args: $args"
+python3 -m sglang.launch_server $args
