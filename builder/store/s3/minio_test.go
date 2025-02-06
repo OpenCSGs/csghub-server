da3d8d519fca8a36bf1463e4e8bbb6edda658651
yiling.ji <yl.ji@opencsg.com>
2025-02-06 02:48:23 +0000

    Merge branch 'refactor/lfs_upload' into 'main'
    LFS upload sha256 validation
    
    See merge request product/starhub/starhub-server!847

--- builder/store/s3/minio_test.go
+++ builder/store/s3/minio_test.go
@@ -1,24 +1,123 @@
 package s3
 
 import (
+	"bytes"
 	"context"
+	"crypto/sha256"
+	"errors"
+	"fmt"
+	"io"
 	"testing"
+
+	"github.com/minio/minio-go/v7"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+	"opencsg.com/csghub-server/_mocks/opencsg.com/csghub-server/builder/store/s3"
 )
 
-// test for useInternalClient
-func Test_useInternalClient(t *testing.T) {
-	c := &minioClient{}
-	ctx := context.Background()
-	if c.useInternalClient(ctx) != false {
-		t.Errorf("should not use internal s3 client if context not defined")
-	}
-	ctxWrongValue := context.WithValue(ctx, "X-OPENCSG-S3-Internal", "test")
-	if c.useInternalClient(ctxWrongValue) != false {
-		t.Errorf("should not use internal s3 client if context value is not 'true'")
-	}
-	ctxWithRightValue := context.WithValue(ctx, "X-OPENCSG-S3-Internal", true)
-	if c.useInternalClient(ctxWithRightValue) != true {
-		t.Errorf("should not use internal s3 client if context value is 'true'")
+func TestMinIO_UploadAndValidate(t *testing.T) {
+	cases := []struct {
+		name           string
+		putFailed      bool
+		checksumFailed bool
+		sizeFailed     bool
+		copyFailed     bool
+		removeFailed   bool
+		err            string
+	}{
+		{name: "success"},
+		{
+			name: "invalid checksum", checksumFailed: true,
+			err: "LFSObject: expected sha256 c3ab8ff13720e8ad9047dd39466b3c8974e592c2fa383d4a3960714caef0c4f2, got a7452118bfc838ee7b2aac14a8bc88c50a1ae4620903c4f8cdd327bb79961899",
+		},
+		{
+			name: "invalid size", sizeFailed: true,
+			err: "LFSObject: expected size 6, got 4",
+		},
+		{name: "put tmp failed", putFailed: true, err: "put failed"},
+		{name: "copy failed", copyFailed: true, err: "copy failed"},
+		{name: "remove failed", removeFailed: true},
 	}
 
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			mockClient := s3.NewMockClient(t)
+			ctx := context.TODO()
+			checksum := fmt.Sprintf("%x", sha256.Sum256([]byte("foobar")))
+			key := checksum[:1] + "/" + checksum[1:2] + "/" + checksum[2:]
+			reader := bytes.NewReader([]byte("foobar"))
+
+			mockClient.EXPECT().PutObject(
+				mock.Anything, "foo", "lfs/tmp/"+key, mock.Anything, int64(6), minio.PutObjectOptions{
+					ContentType:           "application/octet-stream",
+					SendContentMd5:        true,
+					ConcurrentStreamParts: true,
+					NumThreads:            MINIO_PUT_PARALLEL,
+					PartSize:              MINIO_PART_SIZE,
+				}).RunAndReturn(
+				func(
+					ctx context.Context, s1, s2 string,
+					r io.Reader, i int64, poo minio.PutObjectOptions,
+				) (minio.UploadInfo, error) {
+					if c.putFailed {
+						return minio.UploadInfo{}, errors.New("put failed")
+					}
+					if c.checksumFailed {
+						_, err := r.Read(make([]byte, 4))
+						require.NoError(t, err)
+					} else {
+						_, err := r.Read(make([]byte, 6))
+						require.NoError(t, err)
+					}
+					if c.sizeFailed {
+						return minio.UploadInfo{Size: 4}, nil
+					}
+					return minio.UploadInfo{Size: 6}, nil
+				},
+			)
+
+			if !c.putFailed && !c.checksumFailed && !c.sizeFailed {
+				mockClient.EXPECT().CopyObject(mock.Anything, minio.CopyDestOptions{
+					Bucket: "foo",
+					Object: "lfs/" + key,
+				}, minio.CopySrcOptions{
+					Bucket: "foo",
+					Object: "lfs/tmp/" + key,
+				}).RunAndReturn(
+					func(
+						ctx context.Context, cdo minio.CopyDestOptions, cso minio.CopySrcOptions,
+					) (minio.UploadInfo, error) {
+						if c.copyFailed {
+							return minio.UploadInfo{}, errors.New("copy failed")
+						}
+						return minio.UploadInfo{VersionID: "abc"}, nil
+					})
+			}
+
+			if !c.putFailed {
+				mockClient.EXPECT().RemoveObject(
+					mock.Anything, "foo", "lfs/tmp/"+key, minio.RemoveObjectOptions{},
+				).RunAndReturn(func(
+					ctx context.Context, s1, s2 string, roo minio.RemoveObjectOptions,
+				) error {
+					if c.removeFailed {
+						return errors.New("remove failed")
+					}
+					return nil
+				})
+			}
+
+			info, err := uploadAndValidate(
+				ctx, mockClient, "foo", "lfs/"+key,
+				reader, 6,
+			)
+
+			if c.err != "" {
+				require.Equal(t, c.err, err.Error())
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, minio.UploadInfo{VersionID: "abc"}, info)
+			}
+		})
+	}
 }
--- go.mod
+++ go.mod
@@ -21,7 +21,7 @@ require (
 	github.com/golang/gddo v0.0.0-20210115222349-20d68f94ee1f
 	github.com/google/wire v0.6.0
 	github.com/marcboeker/go-duckdb v1.5.6
-	github.com/minio/minio-go/v7 v7.0.66
+	github.com/minio/minio-go/v7 v7.0.84
 	github.com/minio/sha256-simd v1.0.1
 	github.com/naoina/toml v0.1.1
 	github.com/parquet-go/parquet-go v0.24.0
@@ -114,6 +114,7 @@ require (
 	github.com/evanphx/json-patch v5.9.0+incompatible // indirect
 	github.com/facebookgo/clock v0.0.0-20150410010913-600d898af40a // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/go-ini/ini v1.67.0 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/go-sql-driver/mysql v1.8.1 // indirect
@@ -277,7 +278,7 @@ require (
 	github.com/jmespath/go-jmespath v0.4.1-0.20220621161143-b0104c826a24 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
-	github.com/klauspost/compress v1.17.9 // indirect
+	github.com/klauspost/compress v1.17.11 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.9 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
@@ -295,7 +296,7 @@ require (
 	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
-	github.com/rs/xid v1.5.0 // indirect
+	github.com/rs/xid v1.6.0 // indirect
 	github.com/sirupsen/logrus v1.9.3
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/tjfoc/gmsm v1.4.1 // indirect
@@ -308,12 +309,12 @@ require (
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/arch v0.12.0 // indirect
-	golang.org/x/crypto v0.31.0
-	golang.org/x/net v0.32.0 // indirect
+	golang.org/x/crypto v0.32.0
+	golang.org/x/net v0.34.0 // indirect
 	golang.org/x/oauth2 v0.24.0 // indirect
 	golang.org/x/sync v0.10.0 // indirect
 	golang.org/x/sys v0.29.0 // indirect
-	golang.org/x/term v0.27.0 // indirect
+	golang.org/x/term v0.28.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
 	golang.org/x/time v0.6.0 // indirect
 	golang.org/x/tools v0.28.0
