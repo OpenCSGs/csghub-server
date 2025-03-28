db749559f673a132ef30cabaf20be55e7eff135f
Haihui.Wang <wanghh2000@163.com>
2025-03-28 05:45:43 +0000

    [runtime] add RuntimeFramework structure in migration

--- builder/store/database/migrations/20240515100435_create_runtime_framework.go
+++ builder/store/database/migrations/20240515100435_create_runtime_framework.go
@@ -4,14 +4,22 @@ import (
 	"context"
 
 	"github.com/uptrace/bun"
-	"opencsg.com/csghub-server/builder/store/database"
 )
+
+type RuntimeFramework struct {
+	ID           int64  `bun:",pk,autoincrement" json:"id"`
+	FrameName    string `bun:",notnull" json:"frame_name"`
+	FrameVersion string `bun:",notnull" json:"frame_version"`
+	FrameImage   string `bun:",nullzero" json:"frame_image"`
+	Enabled      int64  `bun:",notnull" json:"enabled"`
+	times
+}
 
 func init() {
 	Migrations.MustRegister(func(ctx context.Context, db *bun.DB) error {
-		err := createTables(ctx, db, database.RuntimeFramework{})
+		err := createTables(ctx, db, RuntimeFramework{})
 		return err
 	}, func(ctx context.Context, db *bun.DB) error {
-		return dropTables(ctx, db, database.RuntimeFramework{})
+		return dropTables(ctx, db, RuntimeFramework{})
 	})
 }
---
+++ builder/store/database/migrations/20250328033800_update_runtime_framework_columns.up.sql
@@ -0,0 +1,17 @@
+SET statement_timeout = 0;
+
+--bun:split
+
+ALTER TABLE runtime_frameworks ALTER COLUMN frame_image DROP NOT NULL;
+
+--bun:split
+
+ALTER TABLE runtime_frameworks ALTER COLUMN frame_npu_image DROP NOT NULL;
+
+--bun:split
+
+ALTER TABLE runtime_frameworks ALTER COLUMN frame_cpu_image DROP NOT NULL;
+
+--bun:split
+
+ALTER TABLE runtime_frameworks ALTER COLUMN engine_args DROP NOT NULL;
