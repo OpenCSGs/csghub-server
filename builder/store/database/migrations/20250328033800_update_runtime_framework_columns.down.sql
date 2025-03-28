1d73decc7d665c7792a04a782346e11b806fd38f
Sean <38311363@qq.com>
2025-03-28 14:14:59 +0800

    fix(migrations): remove NOT NULL constraint on frame_npu_image
    - Remove NOT NULL constraint on frame_npu_image
    - Adjust frame_cpu_image accordingly

--- builder/store/database/migrations/20250328033800_update_runtime_framework_columns.down.sql
+++ builder/store/database/migrations/20250328033800_update_runtime_framework_columns.down.sql
@@ -6,10 +6,6 @@ ALTER TABLE runtime_frameworks ALTER COLUMN frame_image SET NOT NULL;
 
 --bun:split
 
-ALTER TABLE runtime_frameworks ALTER COLUMN frame_npu_image SET NOT NULL;
-
---bun:split
-
 ALTER TABLE runtime_frameworks ALTER COLUMN frame_cpu_image SET NOT NULL;
 
 --bun:split
--- builder/store/database/migrations/20250328033800_update_runtime_framework_columns.up.sql
+++ builder/store/database/migrations/20250328033800_update_runtime_framework_columns.up.sql
@@ -6,10 +6,6 @@ ALTER TABLE runtime_frameworks ALTER COLUMN frame_image DROP NOT NULL;
 
 --bun:split
 
-ALTER TABLE runtime_frameworks ALTER COLUMN frame_npu_image DROP NOT NULL;
-
---bun:split
-
 ALTER TABLE runtime_frameworks ALTER COLUMN frame_cpu_image DROP NOT NULL;
 
 --bun:split
