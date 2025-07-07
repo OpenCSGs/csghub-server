60585be21179cacac4bc96aa7b59912bbd38db59
QinYuuuu <1328738411@qq.com>
2025-07-07 16:44:08 +0800

    feat(api): enhance model and MCP server routes

--- api/router/api_mcp.go
+++ api/router/api_mcp.go
@@ -13,6 +13,7 @@ func CreateMCPServerRoutes(
 	mcpServerHandler *handler.MCPServerHandler,
 	repoCommonHandler *handler.RepoHandler) {
 	mcpGroup := apiGroup.Group("/mcps")
+	mcpGroup.Use(middleware.RepoType(types.MCPServerRepo))
 	{
 		// mcp server handler functions
 		mcpGroup.GET("", mcpServerHandler.Index)
@@ -24,28 +25,30 @@ func CreateMCPServerRoutes(
 		mcpGroup.PUT("/:namespace/:name", middlewareCollection.Auth.NeedLogin, mcpServerHandler.Update)
 		mcpGroup.POST("/:namespace/:name/deploys", middlewareCollection.Auth.NeedLogin, mcpServerHandler.Deploy)
 
+	}
+	{
 		// repo common handler functions
-		mcpGroup.GET("/:namespace/:name/branches", middleware.RepoType(types.MCPServerRepo), repoCommonHandler.Branches)
-		mcpGroup.GET("/:namespace/:name/tags", middleware.RepoType(types.MCPServerRepo), repoCommonHandler.Tags)
-		mcpGroup.POST("/:namespace/:name/preupload/:revision", middleware.RepoType(types.MCPServerRepo), repoCommonHandler.Preupload)
-		mcpGroup.POST("/:namespace/:name/tags/:category", middleware.RepoType(types.MCPServerRepo), repoCommonHandler.UpdateTags)
-		mcpGroup.GET("/:namespace/:name/last_commit", middleware.RepoType(types.MCPServerRepo), repoCommonHandler.LastCommit)
-		mcpGroup.GET("/:namespace/:name/commit/:commit_id", middleware.RepoType(types.MCPServerRepo), repoCommonHandler.CommitWithDiff)
-		mcpGroup.POST("/:namespace/:name/commit/:revision", middleware.RepoType(types.MCPServerRepo), repoCommonHandler.CommitFiles)
-		mcpGroup.GET("/:namespace/:name/remote_diff", middleware.RepoType(types.MCPServerRepo), repoCommonHandler.RemoteDiff)
-		mcpGroup.GET("/:namespace/:name/tree", middleware.RepoType(types.MCPServerRepo), repoCommonHandler.Tree)
-		mcpGroup.GET("/:namespace/:name/refs/:ref/tree/*path", middleware.RepoType(types.MCPServerRepo), repoCommonHandler.TreeV2)
-		mcpGroup.GET("/:namespace/:name/refs/:ref/logs_tree/*path", middleware.RepoType(types.MCPServerRepo), repoCommonHandler.LogsTree)
-		mcpGroup.GET("/:namespace/:name/commits", middleware.RepoType(types.MCPServerRepo), repoCommonHandler.Commits)
-		mcpGroup.POST("/:namespace/:name/raw/*file_path", middlewareCollection.Auth.NeedLogin, middleware.RepoType(types.MCPServerRepo), repoCommonHandler.CreateFile)
-		mcpGroup.GET("/:namespace/:name/raw/*file_path", middleware.RepoType(types.MCPServerRepo), repoCommonHandler.FileRaw)
-		mcpGroup.GET("/:namespace/:name/blob/*file_path", middleware.RepoType(types.MCPServerRepo), repoCommonHandler.FileInfo)
-		mcpGroup.GET("/:namespace/:name/download/*file_path", middleware.RepoType(types.MCPServerRepo), repoCommonHandler.DownloadFile)
-		mcpGroup.GET("/:namespace/:name/resolve/*file_path", middleware.RepoType(types.MCPServerRepo), repoCommonHandler.ResolveDownload)
-		mcpGroup.PUT("/:namespace/:name/raw/*file_path", middlewareCollection.Auth.NeedLogin, middleware.RepoType(types.MCPServerRepo), repoCommonHandler.UpdateFile)
-		mcpGroup.POST("/:namespace/:name/update_downloads", middleware.RepoType(types.MCPServerRepo), repoCommonHandler.UpdateDownloads)
-		mcpGroup.PUT("/:namespace/:name/incr_downloads", middlewareCollection.Auth.NeedLogin, middleware.RepoType(types.MCPServerRepo), repoCommonHandler.IncrDownloads)
-		mcpGroup.POST("/:namespace/:name/upload_file", middlewareCollection.Auth.NeedLogin, middleware.RepoType(types.MCPServerRepo), repoCommonHandler.UploadFile)
-		mcpGroup.POST("/:namespace/:name/mirror_from_saas", middlewareCollection.Auth.NeedLogin, middleware.RepoType(types.MCPServerRepo), repoCommonHandler.MirrorFromSaas)
+		mcpGroup.GET("/:namespace/:name/branches", repoCommonHandler.Branches)
+		mcpGroup.GET("/:namespace/:name/tags", repoCommonHandler.Tags)
+		mcpGroup.POST("/:namespace/:name/preupload/:revision", repoCommonHandler.Preupload)
+		mcpGroup.POST("/:namespace/:name/tags/:category", repoCommonHandler.UpdateTags)
+		mcpGroup.GET("/:namespace/:name/last_commit", repoCommonHandler.LastCommit)
+		mcpGroup.GET("/:namespace/:name/commit/:commit_id", repoCommonHandler.CommitWithDiff)
+		mcpGroup.POST("/:namespace/:name/commit/:revision", repoCommonHandler.CommitFiles)
+		mcpGroup.GET("/:namespace/:name/remote_diff", repoCommonHandler.RemoteDiff)
+		mcpGroup.GET("/:namespace/:name/tree", repoCommonHandler.Tree)
+		mcpGroup.GET("/:namespace/:name/refs/:ref/tree/*path", repoCommonHandler.TreeV2)
+		mcpGroup.GET("/:namespace/:name/refs/:ref/logs_tree/*path", repoCommonHandler.LogsTree)
+		mcpGroup.GET("/:namespace/:name/commits", repoCommonHandler.Commits)
+		mcpGroup.POST("/:namespace/:name/raw/*file_path", middlewareCollection.Auth.NeedLogin, repoCommonHandler.CreateFile)
+		mcpGroup.GET("/:namespace/:name/raw/*file_path", repoCommonHandler.FileRaw)
+		mcpGroup.GET("/:namespace/:name/blob/*file_path", repoCommonHandler.FileInfo)
+		mcpGroup.GET("/:namespace/:name/download/*file_path", repoCommonHandler.DownloadFile)
+		mcpGroup.GET("/:namespace/:name/resolve/*file_path", repoCommonHandler.ResolveDownload)
+		mcpGroup.PUT("/:namespace/:name/raw/*file_path", middlewareCollection.Auth.NeedLogin, repoCommonHandler.UpdateFile)
+		mcpGroup.POST("/:namespace/:name/update_downloads", repoCommonHandler.UpdateDownloads)
+		mcpGroup.PUT("/:namespace/:name/incr_downloads", middlewareCollection.Auth.NeedLogin, repoCommonHandler.IncrDownloads)
+		mcpGroup.POST("/:namespace/:name/upload_file", middlewareCollection.Auth.NeedLogin, repoCommonHandler.UploadFile)
+		mcpGroup.POST("/:namespace/:name/mirror_from_saas", middlewareCollection.Auth.NeedLogin, repoCommonHandler.MirrorFromSaas)
 	}
 }
