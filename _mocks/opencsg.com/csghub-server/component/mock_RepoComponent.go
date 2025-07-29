c381162fbf636c39c1e25036a3dc5e2a6ea71647
Xiang Zhen Gan <m1353825@163.com>
2025-07-29 10:41:34 +0800

    fix test

--- _mocks/opencsg.com/csghub-server/component/mock_RepoComponent.go
+++ _mocks/opencsg.com/csghub-server/component/mock_RepoComponent.go
@@ -8,6 +8,8 @@ import (
 	deploy "opencsg.com/csghub-server/builder/deploy"
 	database "opencsg.com/csghub-server/builder/store/database"
 
+	gin "github.com/gin-gonic/gin"
+
 	gitserver "opencsg.com/csghub-server/builder/git/gitserver"
 
 	io "io"
@@ -2912,6 +2914,64 @@ func (_c *MockRepoComponent_MirrorFromSaas_Call) Return(_a0 error) *MockRepoComp
 }
 
 func (_c *MockRepoComponent_MirrorFromSaas_Call) RunAndReturn(run func(context.Context, string, string, string, types.RepositoryType) error) *MockRepoComponent_MirrorFromSaas_Call {
+	_c.Call.Return(run)
+	return _c
+}
+
+// ParseNDJson provides a mock function with given fields: ctx
+func (_m *MockRepoComponent) ParseNDJson(ctx *gin.Context) (*types.CommitFilesReq, error) {
+	ret := _m.Called(ctx)
+
+	if len(ret) == 0 {
+		panic("no return value specified for ParseNDJson")
+	}
+
+	var r0 *types.CommitFilesReq
+	var r1 error
+	if rf, ok := ret.Get(0).(func(*gin.Context) (*types.CommitFilesReq, error)); ok {
+		return rf(ctx)
+	}
+	if rf, ok := ret.Get(0).(func(*gin.Context) *types.CommitFilesReq); ok {
+		r0 = rf(ctx)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*types.CommitFilesReq)
+		}
+	}
+
+	if rf, ok := ret.Get(1).(func(*gin.Context) error); ok {
+		r1 = rf(ctx)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// MockRepoComponent_ParseNDJson_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'ParseNDJson'
+type MockRepoComponent_ParseNDJson_Call struct {
+	*mock.Call
+}
+
+// ParseNDJson is a helper method to define mock.On call
+//   - ctx *gin.Context
+func (_e *MockRepoComponent_Expecter) ParseNDJson(ctx interface{}) *MockRepoComponent_ParseNDJson_Call {
+	return &MockRepoComponent_ParseNDJson_Call{Call: _e.mock.On("ParseNDJson", ctx)}
+}
+
+func (_c *MockRepoComponent_ParseNDJson_Call) Run(run func(ctx *gin.Context)) *MockRepoComponent_ParseNDJson_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		run(args[0].(*gin.Context))
+	})
+	return _c
+}
+
+func (_c *MockRepoComponent_ParseNDJson_Call) Return(_a0 *types.CommitFilesReq, _a1 error) *MockRepoComponent_ParseNDJson_Call {
+	_c.Call.Return(_a0, _a1)
+	return _c
+}
+
+func (_c *MockRepoComponent_ParseNDJson_Call) RunAndReturn(run func(*gin.Context) (*types.CommitFilesReq, error)) *MockRepoComponent_ParseNDJson_Call {
 	_c.Call.Return(run)
 	return _c
 }
