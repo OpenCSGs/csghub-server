ed14d2a248e9329e9e7d1827478e78ce919b9c96
Haihui.Wang <wanghh2000@163.com>
2025-08-29 02:00:22 +0000

    [dataviewer] Convert and preview json/csv file fail

--- builder/git/gitserver/gitaly/common.go
+++ builder/git/gitserver/gitaly/common.go
@@ -9,12 +9,14 @@ import (
 )
 
 func (c *Client) BuildRelativePath(ctx context.Context, repoType types.RepositoryType, namespace, name string) (string, error) {
-	repo, err := c.repoStore.FindByPath(ctx, repoType, namespace, name)
-	if err != nil {
-		return "", fmt.Errorf("failed to find repository: %w", err)
-	}
-	if repo.Hashed {
-		return common.BuildHashedRelativePath(repo.ID) + ".git", nil
+	if repoType != types.RepositoryType(c.config.RepoTemplate.EmptyRepoType) {
+		repo, err := c.repoStore.FindByPath(ctx, repoType, namespace, name)
+		if err != nil {
+			return "", fmt.Errorf("failed to find repository: %w", err)
+		}
+		if repo.Hashed {
+			return common.BuildHashedRelativePath(repo.ID) + ".git", nil
+		}
 	}
 	repoTypeS := fmt.Sprintf("%ss", string(repoType))
 	return common.BuildRelativePath(repoTypeS, namespace, name) + ".git", nil
