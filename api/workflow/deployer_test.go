5ca4875f42bbb9b92150b87dd0a19e46072fe3c3
cemeng <cemengzhang@yntengyun.com>
2026-02-04 10:18:00 +0800

    fix agent lint error

--- api/workflow/deployer_test.go
+++ api/workflow/deployer_test.go
@@ -145,6 +145,7 @@ func TestDeployWorkflowSuccess(t *testing.T) {
 
 	mockDeployTaskStore.EXPECT().GetDeployTask(mock.Anything, runTask.ID).Return(runTask, nil)
 	mockDeployTaskStore.EXPECT().GetDeployByID(mock.Anything, mock.Anything).Return(deploy, nil).Maybe()
+	mockDeployTaskStore.EXPECT().GetLastTaskByType(mock.Anything, runTask.Deploy.ID, runTask.TaskType).Return(runTask, nil)
 
 	runTask.Status = common.Pending
 	mockImageRunner.EXPECT().Run(mock.Anything, mock.Anything).Return(&types.RunResponse{
