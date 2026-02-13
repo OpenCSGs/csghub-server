a09b217fc17aa73c460c4125d8f1e5820a3dc9b5
QinYuuuu <1328738411@qq.com>
2026-02-13 16:30:41 +0800

    fix lint

--- builder/deploy/cluster/cluster_manager.go
+++ builder/deploy/cluster/cluster_manager.go
@@ -467,7 +467,7 @@ func collectNodeResource(node v1.Node, config *config.Config) types.NodeResource
 	}
 
 	gpuModelVendor, gpuModel := getGpuTypeAndVendor(node.Labels[xpuTypeLabel], xpuCapacityLabel)
-	vXPUs := collectNodeVXPU(node, config)
+	vXPUs := collectNodeVXPU(node)
 	nodeResourceInfo := types.NodeResourceInfo{
 		NodeName:   node.Name,
 		NodeStatus: status,
@@ -512,7 +512,7 @@ func collectNodePodsResource(pod v1.Pod, config *config.Config, nodeResource *ty
 			SvcName:      pod.Labels[rtypes.KnativeConfigLabelName],
 			WorkflowName: pod.Labels[rtypes.WorkflowConfigLabelName],
 			ClusterNode:  pod.Spec.NodeName,
-			VXPUs:        collectPodVXPU(pod, config),
+			VXPUs:        collectPodVXPU(pod),
 		})
 	}
 }
