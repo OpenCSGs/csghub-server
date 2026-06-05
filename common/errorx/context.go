efa4fa6fd15044c03468d986cd5f51d76f6be99a
Lei Da <ld_leida@hotmail.com>
2026-06-05 11:09:20 +0800

    Secure admin JWT refresh

--- common/errorx/context.go
+++ common/errorx/context.go
@@ -1,13 +1,13 @@
 package errorx
 
-type context map[string]interface{}
+type context map[string]any
 
 func Ctx() context {
-	ext := make(map[string]interface{})
+	ext := make(map[string]any)
 	return ext
 }
 
-func (ctx context) Set(key string, value interface{}) context {
+func (ctx context) Set(key string, value any) context {
 	ctx[key] = value
 	return ctx
 }
